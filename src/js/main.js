923f612f91f2d0364e57e63652b730e0e081ef8b
Grace Lee <eunheh@gmail.com>
2016-10-18 14:01:39 -0400

    corrected text box and language selector

--- src/js/main.js
+++ src/js/main.js
@@ -5,36 +5,43 @@ function render(item) {
   // console.log(item);
   if (item.type === "text") {
     return `<div class="formBox">
-      <i class="inside fa ${item.icon}" aria-hidden="true" class="formIcon"></i>
-      <input class="inputBar" type="text" placeholder="${item.label}">
-      </div>`
+              <i class="inside fa ${item.icon}" aria-hidden="true" class="formIcon"></i>
+              <input class="inputBar" type="text" placeholder="${item.label}">
+            </div>`
    } if (item.type === "email") {
        return `<div class="formBox">
-         <i class="inside fa ${item.icon}" aria-hidden="true" class="formIcon"></i>
-         <input class="inputBar" type="text" placeholder="${item.label}">
-         </div>`
+                 <i class="inside fa ${item.icon}" aria-hidden="true" class="formIcon"></i>
+                 <input class="inputBar" type="text" placeholder="${item.label}">
+               </div>`
    } if (item.type === "select") {
        return `<div class="selectBox">
-         <select class="selectBar">
-            <option value="">Select language...</option>
-            <option value="${item.options[0]}">English</option>
-            <option value="${item.options[1]}">French</option>
-            <option value="${item.options[2]}">Spanish</option>
-            <option value="${item.options[3]}">Chinese</option>
-            <option value="${item.options[4]}">Japanese</option>
-         </div>`
+                 <select class="selectBar">
+                 <option>Select Language...</option>
+                 ${item.options.map(function(option){
+                   return `<option value="${option.value}">${option.label}</option>`
+                 })}
+                 </select>
+               </div>`
    } if (item.type === "textarea") {
        return `<div class="textBox">
-         <i class="inside fa ${item.icon}" aria-hidden="true" class="formIcon"></i>
-         <input class="comment" type="text" placeholder="${item.label}">
-         </div>`
+                 <i class="inside fa ${item.icon}" aria-hidden="true" class="formIcon"></i>
+                 <textarea class="comment" type="text" placeholder="${item.label}"></textarea>
+               </div>`
    } if (item.type === "tel") {
        return `<div class="formBox">
-         <i class="inside fa ${item.icon}" aria-hidden="true" class="formIcon"></i>
-         <input class="inputBar" type="text" placeholder="${item.label}">
-         </div>`
+                 <i class="inside fa ${item.icon}" aria-hidden="true" class="formIcon"></i>
+                 <input class="inputBar" type="text" placeholder="${item.label}">
+               </div>`
     };
+
 };
+
+// var lang = item.options[0];
+// for (var i = 0; i < 16; i++) {
+//   var selectLang = item.options[i]);
+//   return selectLang
+// }
+
 
 function processItems(data) {
   data.forEach(function(item) {
@@ -51,3 +58,10 @@ function getItems () {
 };
 
 getItems()
+
+// <option value="">Select language...</option>
+// <option value="${item.options[0]}">English</option>
+// <option value="${item.options[1]}">French</option>
+// <option value="${item.options[2]}">Spanish</option>
+// <option value="${item.options[3]}">Chinese</option>
+// <option value="${item.options[4]}">Japanese</option>
